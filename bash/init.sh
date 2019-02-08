fecc91747576c79f79c025299f4e044ce7014739
Anton Shiryaev <anton.o.shiryaev@gmail.com>
2019-02-08 13:49:19 +0300

    Improve sourcing order, add keyboard layout

--- bash/init.sh
+++ bash/init.sh
@@ -5,6 +5,7 @@
 [ -f /usr/local/etc/bash_completion ] && . /usr/local/etc/bash_completion
 
 source "$CUSTOM_PATH/bash/settings.sh"
+source "$CUSTOM_PATH/bash/profile.sh"
 source "$CUSTOM_PATH/bash/alias.sh"
 source "$CUSTOM_PATH/bash/grc.sh"
 source "$CUSTOM_PATH/bash/prompt.sh"
