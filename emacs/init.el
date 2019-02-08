8d0837c550891b9079548e8a856dcf91cc7b83cf
Anton Shiryaev <anton.o.shiryaev@gmail.com>
2019-02-08 12:52:48 +0300

    Use global variable to enable/disable Evil mode

---
+++ emacs/init.el
@@ -0,0 +1,13 @@
+(setq EVIL_ENABLED nil)
+
+(load (concat emacs-custom-path "evil.el"))
+
+(load (concat emacs-custom-path "behavior.el"))
+
+(load (concat emacs-custom-path "display.el"))
+
+(load (concat emacs-custom-path "files.el"))
+
+(load (concat emacs-custom-path "code.el"))
+
+(load (concat emacs-custom-path "modes.el"))
