7f0bf16d5b94478562758e12be0a0fa50c2cb6b2
Anton Shiryaev <anton.o.shiryaev@gmail.com>
2019-03-08 12:36:24 +0300

    Install base16-shell during set-up

--- install.sh
+++ install.sh
@@ -1,10 +1,13 @@
 #! /usr/bin/env bash
 cd deployed/home
+
 GLOBIGNORE=".:.."
 for i in .*; do 
     mv ~/$i{,.bak};
     ln -sf $(pwd)/$i ~/$i;
  done
+
+git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
 
 echo Old configuration files have not been overwritten
 echo Mathematica, iTerm, Emacs, IDEA configs etc cannot be installed automatically
